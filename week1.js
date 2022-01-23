0654b9af722976b96879f1dea3e71ed475ee26cd
deepthi3101 <98285554+deepthi3101@users.noreply.github.com>
2022-01-24 03:38:14 +0530

    commit

--- week1.js
+++ week1.js
@@ -1,7 +1,11 @@
-const http=require('http');
-const requestListener=function(req,res){
-    res.writeHead(200);
-    res.end('Hello World');
-}
-const server=http.createServer(requestListener);
-server.listen(5000);
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
